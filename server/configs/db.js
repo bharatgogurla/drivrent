20e9b5a2b62f1b3d9aff40974143571e8f4e2680
Bharat Gogurla <bharatgogurla@gmail.com>
2026-07-13 21:49:15 +0530

    feat(user): add user schema

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async ()=>{
     try{
         mongoose.connection.on('connected', ()=> console.log("Database Connected"))
-     await mongoose.connect(`${process.env.MONGODB_URI}/drivrent`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/drivrent`)
     } catch (error) {
         console.log(error)
     }
