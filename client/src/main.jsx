42a3a6e6c1e25415cb23a8134cc3598a5412f6ba
Bharat Gogurla <bharatgogurla@gmail.com>
2026-07-17 01:42:41 +0530

    feat(context): add AppContext for global state management

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { AppProvider } from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </BrowserRouter>,
 );
