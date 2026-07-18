b04285f11199806dae0e448ba7624711fa065a43
Bharat Gogurla <bharatgogurla@gmail.com>
2026-07-18 11:05:15 +0530

    feat(home): fetch featured cars from backend

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -68,7 +68,7 @@ export const getUserData = async (req, res) => {
 export const getCars = async (req, res) => {
   try {
     const cars = await Car.find({ isAvailable: true });
-    res.json({ success: true, message: cars });
+    res.json({ success: true, cars });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
