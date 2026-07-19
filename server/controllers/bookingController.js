5221ac52a0f1442feeeb95a1b8d5e85c691ca868
Bharat Gogurla <bharatgogurla@gmail.com>
2026-07-19 16:26:18 +0530

    fix(bookings): correct user bookings query and response

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -78,13 +78,15 @@ export const createBooking = async (req, res) => {
 export const getUserBookings = async (req, res) => {
   try {
     const { _id } = req.user;
-    const bookings = (await Booking.find({ user: _id }).populate("car")).Sort({
-      createdAt: -1,
-    });
+
+    const bookings = await Booking.find({ user: _id })
+      .populate("car")
+      .sort({ createdAt: -1 });
+
     res.json({ success: true, bookings });
   } catch (error) {
     console.log(error.message);
-    res.json({ sucess: false, message: error.message });
+    res.json({ success: false, message: error.message });
   }
 };
 
